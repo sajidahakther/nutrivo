ab58fc78ad47d1e9d43b4b921a9f2e14d650d07d
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-03-30 17:07:21 +0000

    basic overview header

--- nutrivo_app/src/components/header.js
+++ nutrivo_app/src/components/header.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
-  return <Text style={textStyle}>Overview</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+    <Text style={textStyle}>Overview</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center'
+  },
   textStyle: {
-    fontSize: 30
+    fontSize: 30,
+    marginTop: 70,
+    marginBottom: 20,
+    color: '#000000'
   }
 
 };
