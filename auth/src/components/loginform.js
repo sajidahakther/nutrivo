79805cd50acdd877c4cdc976370457ee89d21c79
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-03 17:19:32 +0100

    handled error of auth failing

--- auth/src/components/loginform.js
+++ auth/src/components/loginform.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  // singing user in, authenticate user with firebase
+  onButtonPress() {
+    const { email, password } = this.state;
+    // using asynchronous methods to return a promise
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      // if the sign in process fails, create an account
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          // if user fails to sign in AND create account, throw error
+          .catch(() => {
+            // update the state and error message
+            this.setState({ error: 'Authentication failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -26,13 +44,28 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        {/*  when authentication fails, show error message here */}
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button> Login </Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Login
+          </Button>
         </CardSection>
 
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
