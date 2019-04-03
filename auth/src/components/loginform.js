7d042eb4b730e7c64f1cadfe80599abc8e1743a4
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-03 18:14:56 +0100

    handled login success and failure

--- auth/src/components/loginform.js
+++ auth/src/components/loginform.js
@@ -14,15 +14,32 @@ class LoginForm extends Component {
     this.setState({ error: '', loading: true });
     // using asynchronous methods to return a promise
     firebase.auth().signInWithEmailAndPassword(email, password)
-      // if the sign in process fails, create an account
+      // execute this method if the user successfully signs in
+      .then(this.loginSuccess.bind(this))
+      // if the sign in process fails, create a new account
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          // if user fails to sign in AND create account, throw error
-          .catch(() => {
-            // update the state and error message
-            this.setState({ error: 'Authentication failed.' });
-          });
+          // execute this method if the user successfully creates an account
+          .then(this.loginSuccess.bind(this))
+          // execute this method if the user fails to login AND create new account
+          .catch(this.loginFail.bind(this));
       });
+  }
+
+  loginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false, // clear spinner
+      error: '' // clear pending error message
+    });
+  }
+
+  loginFail() {
+    this.setState({
+      loading: false, // clear spinner
+      error: 'Authentication failed.' // show error message
+    });
   }
 
   // show spinner when button is pressed and auth process is loading, else show the 'login' button
