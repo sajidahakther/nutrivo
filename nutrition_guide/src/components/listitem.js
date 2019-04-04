9244d50124226c7a82053c02cee3c889c2c71e56
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-04 13:52:22 +0100

    updated, need to fix some bugs

--- nutrition_guide/src/components/listitem.js
+++ nutrition_guide/src/components/listitem.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { nutrientName } = styles;
-    const { nutrient } = this.props.library;
+    const { id, nutrient } = this.props.library;
 
     return (
-      <CardSection>
-        <Text style={nutrientName}>
-          {nutrient}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={nutrientName}>
+              {nutrient}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -24,4 +44,12 @@ const styles = {
   }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+
+/* using connect to modify what data is show in the listitem
+returned actions will be dispatched to the redux store
+passes all the actions to my custom components as props */
+export default connect(mapStateToProps, actions)(ListItem);
--- nutrition_guide/src/reducers/selectionreducer.js
+++ nutrition_guide/src/reducers/selectionreducer.js
@@ -1,5 +1,10 @@
 // to check if a particular reducer is selected ot not
 
-export default () => {
-  return null;
-}
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
+};
