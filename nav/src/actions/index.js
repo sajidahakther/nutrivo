4a47c167b89815148cdc676ef113c8fca0c1ce5c
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-08 16:49:33 +0100

    updated

--- nav/src/actions/index.js
+++ nav/src/actions/index.js
@@ -1,8 +1,11 @@
 import firebase from 'firebase';
 
 import {
+  LOGIN_USER_START,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
 } from './types';
 
 export const emailChanged = (text) => {
@@ -19,12 +22,35 @@ export const passwordChanged = (text) => {
   };
 };
 
-// using redux thunk with firebase to handle asynchronous actions
+/* Using redux thunk with firebase to handle asynchronous actions. By using redux thunk,
+I expand the range of values that can be returned from an action creator - particularly
+the function (dispatch) which will be automatically called by redux thunk and will manually
+send an action off to all the reducers in the app (by using this i can do async actions) */
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
+    dispatch({ type: LOGIN_USER_START });
+
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => loginUserFail(dispatch));
       });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
--- nav/src/components/loginform.js
+++ nav/src/components/loginform.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 // redux will be in charge of the value of the email and pass
 class LoginForm extends Component {
@@ -18,6 +19,29 @@ class LoginForm extends Component {
     const { email, password } = this.props;
 
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+      Sign In
+      </Button>
+    );
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: '#FFFFFF' }}>
+          <Text style={styles.errorText}>
+            {this.props.error}
+          </Text>
+        </View>
+      );
+    }
   }
 
   render() {
@@ -42,22 +66,29 @@ class LoginForm extends Component {
         />
         </CardSection>
 
+        {this.renderError()}
+
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-          Sign In
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password
-  };
+const styles = {
+  errorText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: '#FF0000'
+  }
 };
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading } = auth;
+  return { email, password, error, loading };
+};
+
 
 export default connect(mapStateToProps, {
   emailChanged, passwordChanged, loginUser
