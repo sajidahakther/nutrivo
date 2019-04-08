8930a84289f978961eee99f0c813b963344726d9
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-08 13:36:11 +0100

    built login form struct 110

--- nav/src/App.js
+++ nav/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/loginform';
 
 class App extends Component {
   componentWillMount() {
@@ -21,11 +21,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
