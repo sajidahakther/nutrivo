557526332b713fa54842832cb782aac09d05827c
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-08 14:47:28 +0100

    116 update state with pass

--- nav/src/components/loginform.js
+++ nav/src/components/loginform.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 // redux will be in charge of the value of the email and pass
 class LoginForm extends Component {
+
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
   }
 
   render() {
@@ -26,6 +31,8 @@ class LoginForm extends Component {
           secureTextEntry
           label="Password"
           placeholder="password"
+          onChangeText={this.onPasswordChange.bind(this)}
+          value={this.props.password}
         />
         </CardSection>
 
@@ -41,8 +48,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
--- nav/src/reducers/authreducer.js
+++ nav/src/reducers/authreducer.js
@@ -1,16 +1,24 @@
 // ensure action types are spelt correctly so that reducer catches it
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
 
+/* making a new object, taking all the properties from existing
+state object and putting them into that object, then defining the
+property email give it a value of action.payload and put it on
+top of whatever properties were on that state object (overwriting it) */
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      /* making a new object, taking all the properties from existing
-      state object and putting them into that object, then defining the
-      property email give it a value of action.payload and put it on 
-      top of whatever properties were on that state object (overwriting it) */
       return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
     default:
       return state;
   }
