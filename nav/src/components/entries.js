87e0bd460ca712740fc6e610b6defab150babe35
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-04-11 14:07:50 +0100

    transformed obj to arrays

--- nav/src/components/entries.js
+++ nav/src/components/entries.js
@@ -1,14 +1,30 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { entriesFetch } from '../actions';
 
 class Entries extends Component {
-componentWillMount() {
-  this.props.entriesFetch();
-}
+  componentWillMount() {
+    this.props.entriesFetch(); // loads up list of entries
+    this.createDataSource(this.props);
+    }
+
+  // nextProps is the next set of props that the component is rendered with
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ entries }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(entries);
+  }
 
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>apple</Text>
@@ -22,4 +38,14 @@ componentWillMount() {
   }
 }
 
-export default connect(null, { entriesFetch })(Entries);
+/* state.entries is an object that has many key value pairs
+for each key value pair '_.map' run the function. Takes all
+the returned objects (entries) and put it in an array */
+const mapStateToProps = state => {
+  const entries = _.map(state.entries, (val, uid) => {
+      return { ...val, uid };
+  });
+  return { entries };
+};
+
+export default connect(mapStateToProps, { entriesFetch })(Entries);
