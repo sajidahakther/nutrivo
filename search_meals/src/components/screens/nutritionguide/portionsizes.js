e8fe111a2130df3299fc93f78c29b0e3336d2834
Sajidah Akther <sajidah13@hotmail.co.uk>
2019-05-08 23:49:26 +0100

    potion sizes screen complete

--- search_meals/src/components/screens/nutritionguide/portionsizes.js
+++ search_meals/src/components/screens/nutritionguide/portionsizes.js
@@ -1,36 +1,88 @@
-import React, { } from 'react';
+import React, { Component } from 'react';
+import { Image } from 'react-native';
 import {
+  Container,
+  View,
+  DeckSwiper,
+  Card,
+  CardItem,
   Text,
-  View
-} from 'react-native';
-import { Actions } from 'react-native-router-flux';
+  Body
+} from 'native-base';
 
-const PortionSizes = () => {
-  return (
-    <View style={styles.container}>
-      <Text
-        style={styles.welcome}
-        onPress={() => Actions.overviewscreen()}
-      >
-       portion sizes
-      </Text>
-    </View>
-  );
-};
+const cards = [
+  {
+    text: 'Two Cupped Hands (200g)',
+    description: 'The recommended portion for mediterranean vegetables (mushroom, carrot, pepper) and leafy vegetables (lettuce, spinach, chicory)',
+    image: require('../../assets/2hands.png'),
+  },
+  {
+    text: 'One Cupped Hand (15-35g)',
+    description: 'The recommended portion for dried fruits (goji berries, raisins, dates) and nuts such as almonds.',
+    image: require('../../assets/1hand.png'),
+  },
+];
+
+export default class FoodSwiper extends Component {
+  render() {
+    return (
+      <Container>
+        <View>
+          <DeckSwiper
+            dataSource={cards}
+            renderItem={item =>
+              <Card style={{ elevation: 3 }}>
+                <CardItem cardBody>
+                  <Image style={styles.foodImage} source={item.image} />
+                </CardItem>
+                <CardItem header bordered>
+                  <Text style={styles.foodName}>{item.text}</Text>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text style={styles.mealDescription}>{item.description}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text style={styles.instructions}>Swipe left or right to view more</Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            }
+          />
+        </View>
+      </Container>
+    );
+  }
+}
 
 const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F8F8F8',
+  icon: {
+    color: '#FA2133',
+    fontSize: 17
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#000000',
-  }
+  foodName: {
+    color: '#383838'
+  },
+  foodImage: {
+    height: 300,
+    flex: 1
+  },
+  mealDescription: {
+    color: '#707070'
+  },
+  tagText: {
+    fontSize: 14,
+    color: '#575757'
+  },
+  checkIcon: {
+    fontSize: 24,
+    textAlign: 'left',
+    color: '#1CD200'
+  },
+  instructions: {
+      fontStyle: 'italic',
+      color: '#7E7E7E',
+    }
 };
-
-export default PortionSizes;
